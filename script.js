f844eddb9b09a91e6ae29e6929610ea2c3464415
Umana004 <159400472+Umana004@users.noreply.github.com>
2025-04-12 22:08:46 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menu-toggle");
+    const sidebar = document.getElementById("sidebar");
+  
+    menuToggle.addEventListener("click", () => {
+      sidebar.classList.toggle("hidden");
+    });
+  });
+  
